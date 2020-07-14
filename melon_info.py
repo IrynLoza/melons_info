c98610a706fd54ea8e277b551fda85d0a6fd35fd
Iryna Loza <irynloza@gmail.com>
2020-07-13 18:37:13 -0700

    Change function print_nelon

--- melon_info.py
+++ melon_info.py
@@ -2,16 +2,29 @@
 
 
 from melons import melon_names, melon_seedlessness, melon_prices
-
+from collections import defaultdict
 
 def print_melon(name, seedless, price):
     """Print each melon with corresponding attribute information."""
 
-    have_or_have_not = 'have'
+    have_or_have_not = True
     if seedless:
-        have_or_have_not = 'do not have'
+        have_or_have_not = False
 
-    print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
+    #print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
+    #melons = defaultdict(lambda: None)
+    melons = { 
+        melon_names[i]: {
+        "seedless": have_or_have_not,
+        'price': melon_prices[i],
+        'flesh_color': None,
+        'weight': None,
+        'rind_color': None
+        }
+    }
+
+    for (key, value) in melons.items():
+        print(f'{key} {value}')
 
 
 for i in melon_names:
